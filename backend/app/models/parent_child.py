e6820a440c3d8a1bbc6015f4edcfa8a71f010b05
mehdbenguiza <benguizamehdi3@gmail.com>
2026-07-20 20:35:42 +0100

    fix: import String for SQLAlchemy

--- backend/app/models/parent_child.py
+++ backend/app/models/parent_child.py
@@ -1,6 +1,6 @@
 import uuid
 import enum
-from sqlalchemy import Column, ForeignKey, DateTime, Enum, func
+from sqlalchemy import Column, ForeignKey, DateTime, Enum, func, String
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from app.core.database import Base
